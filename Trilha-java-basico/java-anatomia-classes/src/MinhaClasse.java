46c44c35f13e8db974b48ba8ec94804847d4ede3
CaioDillan <caio.ferreirafenty@gmail.com>
2023-06-27 21:26:05 -0300

    declarando variaveis e metodos

--- Trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ Trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,8 +1,17 @@
 public class MinhaClasse {
 
    public static void main (String [] args) {
-    
-       System.out.print ("Olá turma, sejam bem-vindos");
+String primeiroNome = "Caio";
+String segundoNome = "Ferreira";
+
+String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
 }
     
 }
